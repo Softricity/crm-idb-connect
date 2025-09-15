bb4b9d19e025a463eadedc159a5a61e1dbb9b8bc
MrZorawaR <nitishshyoran420@gmail.com>
2025-09-15 11:18:23 +0530

    updated structure

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import HeaderTabs from '@/components/dashboard-components/headerTabs'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <>
+      <HeaderTabs />
+    </>
+  )
+}
