0609a555eb16ff3c0b42f1b741cd2ca74a6f215d
Kaushik Goswami <thekaushikgoswami@gmail.com>
2025-11-19 09:47:12 +0530

    deleted tests

--- backend/test/jest-setup.ts
+++
@@ -1,6 +0,0 @@
-// test/jest-setup.ts
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-// Load the .env file from the root of the backend folder
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
